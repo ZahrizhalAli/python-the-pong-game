cfdb9f71c55d446732d9b0917c0f815505b2af4a
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2022-11-10 17:16:26 +0800

    Add ML to game

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
 import time
+import cv2
+import numpy as np
+from keras.models import load_model
+
+
+
+# load the model
+model = load_model('model.savedmodel')
 
 screen = Screen()
 screen.setup(800, 600)
@@ -21,7 +29,37 @@ screen.onkeypress(l_paddle.go_down, "s")
 screen.onkeypress(r_paddle.go_up, "Up")
 screen.onkeypress(r_paddle.go_down, "Down")
 is_game_on = True
+
+cap=cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+roi_top = 20
+roi_bottom = 300
+roi_right = 300
+roi_left = 600
 while is_game_on:
+    sucess, imgOriginal = cap.read()
+    # Draw ROI Rectangle on frame copy
+    cv2.rectangle(imgOriginal, (roi_left, roi_top), (roi_right, roi_bottom), (0,0,255), 5)
+    # Resize the raw image into (224-height,224-width) pixels.
+    img = cv2.resize(imgOriginal[roi_top:roi_bottom, roi_right:roi_left], (224, 224))
+    # Show the image in a window
+    cv2.imshow('Webcam Image', imgOriginal)
+    # Make the image a numpy array and reshape it to the models input shape.
+    img = np.asarray(img, dtype=np.float32).reshape(1, 224, 224, 3)
+    # Normalize the image array
+    img = (img / 127.5) - 1
+
+    probabilities = model.predict(img)
+
+    index = np.argmax(probabilities)
+    if index == 0:
+        r_paddle.go_up()
+    if index == 1:
+        r_paddle.go_down()
+
+
     time.sleep(ball.move_speed)
     screen.update()
     ball.move()
@@ -53,3 +91,4 @@ while is_game_on:
 
 
 screen.exitonclick()
+
---
+++ model.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Apply data augmentation
+datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = datagen.flow_from_directory(
+        './images//',  # This is the source directory for training images
+        target_size=(224, 224),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+import tensorflow as tf
+
+# Build the model
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(224, 224, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+model.fit(train_generator, epochs=20)
+
+model.save('model_game')
